0177fc51f964bc8c3b7ab0385510dd708ef4e018
Ishan <bori0013@algonquinlive.com>
2021-03-12 23:54:41 -0500

    Lab 4 Finished

---
+++ Controllers/Home/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab4.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+
+        private readonly SchoolCommunityContext ctxt;
+
+        public HomeController(SchoolCommunityContext context)
+        {
+            ctxt = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class Student
+    {
+
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate
+        {
+            get;
+            set;
+        }
+
+ 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public ICollection<CommunityMembership> CommunityMemberships { get; set; }
+
+
+
+
+    }
+}
